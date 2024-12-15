9241005e58fe8a4468d2c61fe72ef6a1943c5fbd
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2024-12-15 14:50:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -18,13 +18,7 @@
         <br>
         <h3 class="display-4">Tanya Alexandra Vaca Mena</h3>
         <h3 class="display-4">SI8-002</h3>
-        <h1>
-            <?php
-                // Obtén el nombre del host completo
-                $hostname = trim(shell_exec("hostname -f"));
-                echo "Hola Mundo desde $hostname";
-            ?>
-        </h1>
+        <h1>Hola Mundo desde <?php echo shell_exec('hostname -f'); ?></h1>
     </div>
 </body>
 </html>
